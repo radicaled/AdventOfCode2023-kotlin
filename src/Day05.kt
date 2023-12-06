15be3fc6f25c8b734f6cd6d104c4a030877343e5
Arron Washington <arron@theradicaledwards.com>
2023-12-05 22:23:43 -0500

    day05 pt2
    Made a lot of little errors this time that made it worse than it should've been. Shouldn't code so late at night.

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,4 +1,11 @@
-import kotlin.system.measureTimeMillis
+import kotlin.time.measureTime
+
+val bm = Benchmark()
+
+val timePerSeedRef = "timePerSeed".intern()
+val totalTimeRef = "totalTime".intern()
+val timePerSDMRef = "timePerSDM".intern()
+val timePerAlmanacSectionRef = "timePerAlmanacSection".intern()
 
 fun main() {
     fun part1(input: List<String>): Long {
@@ -14,58 +21,122 @@ fun main() {
     fun part2(input: List<String>): Long {
         val almanac = Almanac.parseAlmanac(input)
         var minLocation = Long.MAX_VALUE
-        val pairs = almanac.seeds.zipWithNext()
-        pairs.forEachIndexed { index, it ->
-            println("Working on ${index}")
-            val (start, length) = it
-            
-            var timing = measureTimeMillis {
-                var iterations = 0
-                for (seed in (start..start+length)) {
-                    minLocation = minOf(minLocation, almanac.findSeedLocation(seed))
-                    iterations++
-                    if (iterations > 1_000_000) break
-                } 
-            }
-            println("Timing for part 2: ${timing}ms")
-//            for (seed in (start..start+length)) {
-//                minLocation = minOf(minLocation, almanac.findSeedLocation(seed))
-//            }
-        }        
 
+        val pairs = getSeedPairs(almanac.seeds)
+
+        bm.enabled = false
+        println("Benchmarking enabled: ${bm.enabled}")
+        // Too many iterations here -- there's a very specific way of solving this puzzle I haven't found yet.
+        pairs.forEachIndexed { index, it ->
+            val (start, length) = it
+            val seedRange = getSeedRange(start, length)
+
+            println("Seed range #${index + 1} of ${pairs.size}: ${"%,d".format(length)} seeds")
+
+            val sanity = measureTime {
+                bm.measure(totalTimeRef) {
+                    for (seed in seedRange) {
+                        bm.measure(timePerSeedRef) {
+                            minLocation = minOf(minLocation, almanac.findSeedLocation(seed))
+                        }
+                    }
+                }
+            }
+
+            println("TIME (sanity check) ${sanity.inWholeMilliseconds}ms")
+            if (bm.enabled) {
+                bm.printResults()
+            }
+        }
+        println("FINAL RESULT")
         return minLocation
     }
 
     testSourceDestinationMap()
     testParsing()
+    testSeedPairs()
+    testSeedRange()
     // test if implementation meets criteria from the description, like:
     testSolution(part1(readInput("Day05_test_p1")), 35)
-//    testSolution(part2(readInput("Day05_test_p2")), 281)    
+    testSolution(part2(readInput("Day05_test_p1")), 46)
     
     val input = readInput("Day05")
     part1(input).println()
     part2(input).println()
 }
 
+fun getSeedPairs(input: List<Long>): List<Pair<Long, Long>> {
+    val pairs = mutableListOf<Pair<Long, Long>>()
+    for (i in input.indices step 2) {
+        pairs.add(Pair(input[i], input[i + 1]))
+    }
+
+    return pairs
+}
+
+fun testSeedPairs() {
+    val seeds = listOf(79L, 14L, 55L, 13L)
+    val results = getSeedPairs(seeds)
+
+    check(results.size == 2)
+    check(results[0].first == 79L)
+    check(results[0].second == 14L)
+    check(results[1].first == 55L)
+    check(results[1].second == 13L)
+}
+
+fun getSeedRange(seedStart: Long, seedLength: Long): LongRange {
+    return seedStart..<seedStart + seedLength
+}
+
+fun testSeedRange() {
+    val range = getSeedRange(79L, 14L)
+
+    check(range.first == 79L)
+    check(range.last == 92L)
+}
+
 class AlmanacSection(val from: String, val to: String) {
-    val maps = mutableListOf<SourceDestinationMap>()
-    
+    private val maps = mutableListOf<SourceDestinationMap>()
+    private var mapArray: Array<SourceDestinationMap> = arrayOf()
+
+    fun addMap(map: SourceDestinationMap) {
+        maps.add(map)
+        maps.sortBy { it.source }
+
+        mapArray = maps.toTypedArray()
+    }
+
     fun mapInput(input: Long): Long {
-        val result = maps.firstNotNullOfOrNull { it.mapInput(input) }
+        // Skip iteration if we have SURPASSED THE LIMIT
+        if (input > maps.last().sourceEnd) return input
+
+        val result = bm.measure(timePerAlmanacSectionRef) {
+
+            mapArray.asSequence().filter {
+                it.contains(input)
+            }.firstNotNullOfOrNull { it.mapInput(input) }
+        }
+
         return result ?: input
     }
 }
 
 data class SourceDestinationMap(val source: Long, val destination: Long, val rangeLength: Long) {
+    val sourceEnd: Long = source + (rangeLength - 1)
+
+    fun contains(input: Long): Boolean {
+        return input in source..sourceEnd
+    }
     
     fun mapInput(input: Long): Long? {
-        val isInRange = input >= source && input <= (source + rangeLength)
-        
-        if (!isInRange) return null
-        
-        val offset = input - source
-        
-        return destination + offset        
+        return bm.measure(timePerSDMRef) {
+            if (!contains(input)) return@measure null
+
+            val offset = input - source
+
+            destination + offset
+        }
     }
 }
 
@@ -73,7 +144,7 @@ fun testSourceDestinationMap() {
     val sdm = SourceDestinationMap(destination = 50, source = 98, rangeLength = 2)
     val output1 = sdm.mapInput(98)
     val output2 = sdm.mapInput(99)
-    
+
     check(output1 == 50L) {
         println("${output1} does not == 50")
     }
@@ -81,12 +152,42 @@ fun testSourceDestinationMap() {
     check(output2 == 51L) {
         println("${output1} does not == 51")
     }
+
+    // Checking for overlaps.
+    val sdm1 = SourceDestinationMap(destination =  45, source = 77, rangeLength = 22)
+    val sdm2 = SourceDestinationMap(destination =  81, source = 45, rangeLength = 19)
+    val sdm3 = SourceDestinationMap(destination =  68, source = 64, rangeLength = 13)
+
+    val overlapCheckList = listOf(sdm1, sdm2, sdm3)
+
+    overlapCheckList.forEach { sdm ->
+        val overlap = overlapCheckList
+            .filter { it != sdm }
+            .filter { sdm.sourceEnd == it.source }
+            .firstNotNullOfOrNull { Pair(sdm, it) }
+
+        if (overlap != null) {
+            val (a, b) = overlap
+            throw IllegalStateException("${a} (${a.source}, ${a.sourceEnd}) overlapped with ${b} (${b.source}, ${b.sourceEnd})")
+        }
+
+    }
+
+    val overlappingSdm = overlapCheckList.firstNotNullOfOrNull { sdm ->
+        overlapCheckList
+            .filter { it != sdm }
+            .filter { sdm.sourceEnd == it.source }
+            .firstNotNullOfOrNull { Pair(sdm, it) }
+    }
+    if (overlappingSdm != null) {
+        assert(false) {
+            println("Overlap detected: ${overlappingSdm.first} <=> ${overlappingSdm.second}")
+        }
+    }
 }
 
-data class LookupResult(val type: String, val value: Long)
-
-class Almanac(val seeds: List<Long>, val sections: List<AlmanacSection>) {
-    private val sectionMap: Map<String, AlmanacSection>
+class Almanac(val seeds: List<Long>, sections: List<AlmanacSection>) {
+    val sections: List<AlmanacSection>
     
     companion object {
         private val seedsRegex = Regex("^seeds:")
@@ -128,7 +229,7 @@ class Almanac(val seeds: List<Long>, val sections: List<AlmanacSection>) {
                     val rangeLength = values[2]
                     
                     val sdm = SourceDestinationMap(destination = destination, source = source, rangeLength = rangeLength)
-                    currentSection.maps.add(sdm)
+                    currentSection.addMap(sdm)
                 }
             }
             
@@ -139,38 +240,36 @@ class Almanac(val seeds: List<Long>, val sections: List<AlmanacSection>) {
     }
     
     init {
-        val map = mutableMapOf<String, AlmanacSection>()
-        sections.forEach {
-            map[it.from] = it
+        // Gonna make sure the sections are sorted from start to finish
+        val sortedSections = mutableListOf<AlmanacSection>()
+
+        val seedSection = sections.first { it.from == "seed" }
+        sortedSections.add(seedSection)
+
+        var previousSection: AlmanacSection? = seedSection
+        while (previousSection != null) {
+            val section = sections.find { it.from == previousSection?.to }
+
+            if (section != null) {
+                sortedSections.add(section)
+            }
+
+            previousSection = section
         }
         
-        sectionMap = map
+        this.sections = sortedSections
     }
 
     fun findSeedLocation(seed: Long): Long {
-        var lookupResult = lookup("seed", seed)
-
-        var iterations = 0
-        val maxIterations = sections.size
-        while (lookupResult != null) {
-            if (iterations >= maxIterations) throw IllegalStateException("Too many iterations")
-            iterations++
-
-            if (lookupResult.type == "location") return lookupResult.value
-
-            lookupResult = lookup(lookupResult.type, lookupResult.value)
+        var tableValue = seed
+        for (section in sections) {
+            tableValue = section.mapInput(tableValue)
+            if (seed == 82L) {
+                println("Seed ${seed}: ${section.to} is ${tableValue}")
+            }
         }
 
-        throw IllegalStateException("Did not find Location")
-    }
-
-    fun lookup(type: String, value: Long): LookupResult? {
-//        val section = sections.find { it.from == type }
-        val section = sectionMap[type] ?: return null
-        return LookupResult(
-            type = section.to,
-            value = section.mapInput(value) 
-        )
+        return tableValue
     }
 }
 
--- src/Utils.kt
+++ src/Utils.kt
@@ -2,6 +2,8 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+import kotlin.time.Duration
+import kotlin.time.measureTime
 
 /**
  * Reads lines from the given input txt file.
@@ -31,4 +33,47 @@ fun testSolution(input: Long, expectedOutput: Long) {
  check(input == expectedOutput) {
   println("${input} did not match ${expectedOutput}")
  }
+}
+
+class Benchmark {
+    var enabled = true
+
+    val mapTimesAccumulator = mutableMapOf<String, Duration>()
+    val mapTimesCount = mutableMapOf<String, Double>()
+
+    inline fun <T> measure(name: String, crossinline block: () -> T): T {
+        if (!enabled) return block()
+
+        var result: T
+        val time = measureTime {
+            result = block()
+        }
+
+        val timeAcc = mapTimesAccumulator.getOrPut(name) { Duration.ZERO }
+        val timeCount = mapTimesCount.getOrPut(name) { Double.MIN_VALUE }
+
+        mapTimesAccumulator[name] = timeAcc + time
+        mapTimesCount[name] = timeCount + 1
+
+        return result
+    }
+
+    fun average(name: String): Duration {
+        val timeAcc = mapTimesAccumulator.getOrDefault(name, Duration.ZERO)
+        val timeCount = mapTimesCount.getOrDefault(name, Double.MIN_VALUE)
+
+        return timeAcc / timeCount
+    }
+
+    fun printResults() {
+        fun formatTiming(name: String): String {
+            val d = bm.average(name)
+            return "${name}: ${d.inWholeSeconds}secs / ${d.inWholeMilliseconds}millis / ${d.inWholeMicroseconds}micros / ${d.inWholeNanoseconds}nanos"
+        }
+
+        val keys = mapTimesAccumulator.keys.sorted()
+        for(key in keys) {
+            println(formatTiming(key))
+        }
+    }
 }
